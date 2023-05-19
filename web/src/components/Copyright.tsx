2f82d5a15f631d317641852d965e63783ae214a7
Bia Gavira <biagavirete@gmail.com>
2023-05-18 19:38:17 -0600

    feat: organize components and include link to github oauth

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,5 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">Made with 💜 by <a href="https://github.com/biagavirete" target="_blank" rel="noreferrer" className="underline hover:text-gray-100">biagavirete</a> during NLW</div>
+  );
+}
---
+++ web/src/components/EmptyMemories.tsx
@@ -0,0 +1,9 @@
+export function EmptyMemories() {
+  return (
+    <div className="flex flex-1 items-center justify-center">
+      <p className="text-center leading-relaxed w-[360px]">
+        No memories yet, <a className="underline hover:text-gray-50" href="">start now</a>!
+      </p>
+    </div>
+  );
+}
